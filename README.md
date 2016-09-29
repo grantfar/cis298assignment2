6e5a45a6d58ded8ccf701d04933450420f4fb09d
Grant Farnsworth <grantfar@gmail.com>
2016-09-29 14:08:17 -0400

    added documentation and fixed a few bugs.

--- README.md
+++ README.md
@@ -1,47 +1,20 @@
 # Assignment 2 - Temperature Converter
 ## Due 10-10-2016
 ## Author
+Grant Farnsworth
 
 ## Description
 
-Create a temperature converter application.
-The application should look like the screen shots I have provided.
-The application should do the calculation and display the formula that was used to do the calculation.
-The calculation of the conversion should take place in a separate class
-The application should handle errors gracefully.
-The application should have a different layout for landscape form that looks like the one pictured here.
-The application must retain it's information on the screen when rotated to landscape.
+This app converts temperatures between Celsius, Fahrenheit, Kelvin, and 
+Rankine.
 
-Solution Requirements:
-
-* Separate class for calculation
-* Handle errors gracefully
-* Display Formula and Answer
-* Separate Layout for landscape
-* Retain information across rotation
-
-![alt text](http://barnesbrothers.homeserver.com/cis298/assignmentImages/assignment2a.jpg "Application Portrait Layout")
-![alt text](http://barnesbrothers.homeserver.com/cis298/assignmentImages/assignment2b.jpg "Application Landscape Layout")
 ### Notes
 
-The book does not cover how to do work with Radio Buttons. I will talk about it as needed in class. If you are stuck or have questions about it, ask.
-
-
-## Grading
-| Feature                  | Points |
-| ------------------------ | ------ |
-| Radio Buttons Work       | 10     |
-| Displays Formula         | 10     |
-| Displays Answer          | 15     |
-| Separate Class For Calcs | 10     |
-| Handle Errors            | 10     |
-| Portrait Layout          | 10     |
-| Landscape Layout         | 10     |
-| Retain On Rotation       | 15     |
-| Documentation            | 5      |
-| README                   | 5      |
-| Total                    | 100    |
+only displays a max of 2 decimal places
 
 ## Outside Resources Used
 
+https://en.wikipedia.org/wiki/Conversion_of_units_of_temperature
+
 ## Known Problems, Issues, And/Or Errors in the Program
+UI looks very clusted on screens smaller than 720p;
--- app/src/main/java/edu/kvcc/cis298/cis298assignment2/TemperatureConverter.java
+++ app/src/main/java/edu/kvcc/cis298/cis298assignment2/TemperatureConverter.java
@@ -6,18 +6,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
-import edu.kvcc.cis298.cis298assignment2.Conversion;
 
 import java.text.DecimalFormat;
 
 public class TemperatureConverter extends AppCompatActivity {
 
+    //Bundle Keys
     private static final String KEY_FORMULA = "gdfse4wradsCF74";
     private static final String KEY_CONVERSION = "FASDF5DSA512";
 
@@ -37,8 +36,9 @@ public class TemperatureConverter extends AppCompatActivity {
     private TextView mOutputFormulaTextView;
     private EditText mtempInputEditText;
 
-    private DecimalFormat TwoDidgets = new DecimalFormat("#.##");
-    private Double InputedTemp;
+    //Variables
+    private DecimalFormat TwoDigits = new DecimalFormat("#.##");
+    private Double InputtedTemp;
     private Double ConvertedTemp;
 
 
@@ -66,18 +66,21 @@ public class TemperatureConverter extends AppCompatActivity {
         validateButton();
 
         //Create Listeners
+        //To Radio buttons listener
         mOutputRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
                 validateButton();
             }
         });
+        //From Radio buttons listener
         mInputRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
                 validateButton();
             }
         });
+        //Calculate button listener
         mCalculateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -88,6 +91,7 @@ public class TemperatureConverter extends AppCompatActivity {
         });
     }
 
+    //Enables and disables the button depending if both sets of radio buttons have a selection
     private void validateButton()
     {
         if(mOutputRadioGroup.getCheckedRadioButtonId() != -1 && mInputRadioGroup.getCheckedRadioButtonId() != -1)
@@ -96,48 +100,58 @@ public class TemperatureConverter extends AppCompatActivity {
             mCalculateButton.setEnabled(false);
     }
 
+    //Sets the converted temp and conversion formula to the display TextViews
     private void setResults()
     {
+        //error prevention
         if(mtempInputEditText.getText().toString().isEmpty())
         {
             Toast.makeText(TemperatureConverter.this,R.string.empty_edit_toast,Toast.LENGTH_SHORT).show();
             return;
         }
-        InputedTemp = Double.parseDouble(mtempInputEditText.getText().toString());
 
+        InputtedTemp = Double.parseDouble(mtempInputEditText.getText().toString());
+
+        //nested ifs, Outside if selects what "From" radio button is selected
+        //Inside if selects what "To" radio button is checked.
         if (mInputCelsiusRadioButton.isChecked())
         {
             if (mOutputCelsiusRadioButton.isChecked())
             {
+                //If both the "To" and "From" radio buttons are the same
+                //Display an error message
                 Toast.makeText(TemperatureConverter.this,R.string.same_unit_toast,Toast.LENGTH_SHORT).show();
             }
             else if (mOutputFahrenheitRadioButton.isChecked())
             {
-                ConvertedTemp = Conversion.celsiusToFahrenheit(InputedTemp);
-                mOutputAnswerTextView.setText(InputedTemp + "°C = " +  TwoDidgets.format(ConvertedTemp)+ "°F");
+                //calls correct conversion Method fro Conversions Class
+                ConvertedTemp = Conversion.celsiusToFahrenheit(InputtedTemp);
+                //Formats answer and sets it to the right text view
+                mOutputAnswerTextView.setText(TwoDigits.format(InputtedTemp) + "°C = " +  TwoDigits.format(ConvertedTemp)+ "°F");
+                //Displays the formula for conversion
                 mOutputFormulaTextView.setText(R.string.celsius_to_fahrenheit);
             }
             else if (mOutputKelvinRadioButton.isChecked())
             {
-                ConvertedTemp = Conversion.celsiusToKelvin(InputedTemp);
-                mOutputAnswerTextView.setText(InputedTemp + "°C = " +  TwoDidgets.format(ConvertedTemp)+ "K");
+                ConvertedTemp = Conversion.celsiusToKelvin(InputtedTemp);
+                mOutputAnswerTextView.setText(TwoDigits.format(InputtedTemp) + "°C = " +  TwoDigits.format(ConvertedTemp)+ "K");
                 mOutputFormulaTextView.setText(R.string.celsius_to_kelvin);
             }
             else if (mOutputRankineRadioButton.isChecked())
             {
-                ConvertedTemp = Conversion.celsiusToFahrenheit(InputedTemp);
-                mOutputAnswerTextView.setText(InputedTemp + "°C = " +  TwoDidgets.format(ConvertedTemp)+ "°R");
+                ConvertedTemp = Conversion.celsiusToFahrenheit(InputtedTemp);
+                mOutputAnswerTextView.setText(TwoDigits.format(InputtedTemp) + "°C = " +  TwoDigits.format(ConvertedTemp)+ "°R");
                 mOutputFormulaTextView.setText(R.string.celsius_to_rankine);
             }
         }
 
-
+        //See top
         else if (mInputFahrenheitRadioButton.isChecked())
         {
             if (mOutputCelsiusRadioButton.isChecked())
             {
-                ConvertedTemp = Conversion.fahrenheitToCelsius(InputedTemp);
-                mOutputAnswerTextView.setText(InputedTemp + "°F = " +  TwoDidgets.format(ConvertedTemp)+ "°C");
+                ConvertedTemp = Conversion.fahrenheitToCelsius(InputtedTemp);
+                mOutputAnswerTextView.setText(TwoDigits.format(InputtedTemp) + "°F = " +  TwoDigits.format(ConvertedTemp)+ "°C");
                 mOutputFormulaTextView.setText(R.string.fahrenheit_to_celsius);
             }
             else if (mOutputFahrenheitRadioButton.isChecked())
@@ -146,29 +160,31 @@ public class TemperatureConverter extends AppCompatActivity {
             }
             else if (mOutputKelvinRadioButton.isChecked())
             {
-                ConvertedTemp = Conversion.fahrenheitToKelvin(InputedTemp);
-                mOutputAnswerTextView.setText(InputedTemp + "°F = " +  TwoDidgets.format(ConvertedTemp)+ "K");
+                ConvertedTemp = Conversion.fahrenheitToKelvin(InputtedTemp);
+                mOutputAnswerTextView.setText(TwoDigits.format(InputtedTemp) + "°F = " +  TwoDigits.format(ConvertedTemp)+ "K");
                 mOutputFormulaTextView.setText(R.string.fahrenheit_to_kelvin);
             }
             else if (mOutputRankineRadioButton.isChecked())
             {
-                ConvertedTemp = Conversion.fahrenheitToRankine(InputedTemp);
-                mOutputAnswerTextView.setText(InputedTemp + "°F = " +  TwoDidgets.format(ConvertedTemp)+ "°R");
+                ConvertedTemp = Conversion.fahrenheitToRankine(InputtedTemp);
+                mOutputAnswerTextView.setText(TwoDigits.format(InputtedTemp) + "°F = " +  TwoDigits.format(ConvertedTemp)+ "°R");
                 mOutputFormulaTextView.setText(R.string.fahrenheit_to_rankine);
             }
         }
+
+        //See top
         else if (mInputKelvinRadioButton.isChecked())
         {
             if (mOutputCelsiusRadioButton.isChecked())
             {
-                ConvertedTemp = Conversion.kelvinToCelsius(InputedTemp);
-                mOutputAnswerTextView.setText(InputedTemp + "K = " +  TwoDidgets.format(ConvertedTemp)+ "°C");
+                ConvertedTemp = Conversion.kelvinToCelsius(InputtedTemp);
+                mOutputAnswerTextView.setText(TwoDigits.format(InputtedTemp) + "K = " +  TwoDigits.format(ConvertedTemp)+ "°C");
                 mOutputFormulaTextView.setText(R.string.kelvin_to_celsius);
             }
             else if (mOutputFahrenheitRadioButton.isChecked())
             {
-                ConvertedTemp = Conversion.kelvinToFahrenheit(InputedTemp);
-                mOutputAnswerTextView.setText(InputedTemp + "K = " +  TwoDidgets.format(ConvertedTemp)+ "°F");
+                ConvertedTemp = Conversion.kelvinToFahrenheit(InputtedTemp);
+                mOutputAnswerTextView.setText(TwoDigits.format(InputtedTemp) + "K = " +  TwoDigits.format(ConvertedTemp)+ "°F");
                 mOutputFormulaTextView.setText(R.string.kelvin_to_rankine);
             }
             else if (mOutputKelvinRadioButton.isChecked())
@@ -177,29 +193,31 @@ public class TemperatureConverter extends AppCompatActivity {
             }
             else if (mOutputRankineRadioButton.isChecked())
             {
-                ConvertedTemp = Conversion.kelvinToRankine(InputedTemp);
-                mOutputAnswerTextView.setText(InputedTemp + "K = " +  TwoDidgets.format(ConvertedTemp)+ "°R");
+                ConvertedTemp = Conversion.kelvinToRankine(InputtedTemp);
+                mOutputAnswerTextView.setText(TwoDigits.format(InputtedTemp) + "K = " +  TwoDigits.format(ConvertedTemp)+ "°R");
                 mOutputFormulaTextView.setText(R.string.kelvin_to_rankine);
             }
         }
+
+        //See Top
         else if (mInputRankineRadioButton.isChecked())
         {
             if (mOutputCelsiusRadioButton.isChecked())
             {
-                ConvertedTemp = Conversion.rankineToCelsius(InputedTemp);
-                mOutputAnswerTextView.setText(InputedTemp + "°R = " +  TwoDidgets.format(ConvertedTemp)+ "°C");
+                ConvertedTemp = Conversion.rankineToCelsius(InputtedTemp);
+                mOutputAnswerTextView.setText(TwoDigits.format(InputtedTemp) + "°R = " +  TwoDigits.format(ConvertedTemp)+ "°C");
                 mOutputFormulaTextView.setText(R.string.rankine_to_celsius);
             }
             else if (mOutputFahrenheitRadioButton.isChecked())
             {
-                ConvertedTemp = Conversion.rankineToFahrenheit(InputedTemp);
-                mOutputAnswerTextView.setText(InputedTemp + "°R = " +  TwoDidgets.format(ConvertedTemp)+ "°F");
+                ConvertedTemp = Conversion.rankineToFahrenheit(InputtedTemp);
+                mOutputAnswerTextView.setText(TwoDigits.format(InputtedTemp) + "°R = " +  TwoDigits.format(ConvertedTemp)+ "°F");
                 mOutputFormulaTextView.setText(R.string.rankine_to_fahrenheit);
             }
             else if (mOutputKelvinRadioButton.isChecked())
             {
-                ConvertedTemp = Conversion.rankineToKelvin(InputedTemp);
-                mOutputAnswerTextView.setText(InputedTemp + "°R = " +  TwoDidgets.format(ConvertedTemp)+ "K");
+                ConvertedTemp = Conversion.rankineToKelvin(InputtedTemp);
+                mOutputAnswerTextView.setText(TwoDigits.format(InputtedTemp) + "°R = " +  TwoDigits.format(ConvertedTemp)+ "K");
                 mOutputFormulaTextView.setText(R.string.rankine_to_kelvin);
             }
             else if (mOutputRankineRadioButton.isChecked())
@@ -231,6 +249,7 @@ public class TemperatureConverter extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    //Puts the conversion and formula into the bundle
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
@@ -239,9 +258,11 @@ public class TemperatureConverter extends AppCompatActivity {
         outState.putString(KEY_FORMULA,mOutputFormulaTextView.getText().toString());
     }
 
+    //Gets the conversion and formula into the bundle
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
+
         mOutputAnswerTextView.setText(savedInstanceState.getString(KEY_CONVERSION));
         mOutputFormulaTextView.setText(savedInstanceState.getString(KEY_FORMULA));
     }
